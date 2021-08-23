8d33a332ea36fc1d06d1e0e2a342cd96697c6a85
Maksim Sharipov <m.v.sharipov@gmail.com>
2021-08-24 00:36:38 +0200

    fix(upload): songs api url

--- src/api/Songs.api.ts
+++ src/api/Songs.api.ts
@@ -2,7 +2,7 @@ import { HttpClient } from './HttpClient';
 
 export class SongsApi {
   public static async upload(request: FormData): Promise<unknown> {
-    const { data } = await HttpClient.getInstance().post<unknown>('/songs/analyze', request, {
+    const { data } = await HttpClient.getInstance().post<unknown>('/songs/analysis', request, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
