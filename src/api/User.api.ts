55e433c75baa25dddc1e5e83357998bc3d67a6ac
Brett Dulan <bdulan715@gmail.com>
2021-10-09 13:19:55 -0700

    Merging changes from api and changing api url for now

--- src/api/User.api.ts
+++ src/api/User.api.ts
@@ -7,10 +7,4 @@ export class UserApi {
 
     return data;
   }
-
-  public static async update(user: User): Promise<User> {
-    const { data } = await HttpClient.getInstance().put<User>(`/admin/users/${user.id}`, user);
-
-    return data;
-  }
 }
